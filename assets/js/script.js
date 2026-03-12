b53ff148c1a50acdb0df2375cbbfec0bb44bf96c
mateoherreno <mateoherre@outlook.es>
2026-03-11 23:06:59 -0500

    perf: Bootstrap CSS non-blocking, resize card images (-561KB), favicon-nav.webp 2KB, fetchpriority=high on LCP

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,7 +84,7 @@ function renderNavbar() {
     el.innerHTML = `
     <nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
       <div class="container-fluid">
-        <a class="navbar-brand" href="${base || '/'}"><img class="icono-nav" src="${base}assets/img/favicon.png" alt="Plomeros H2O Medellín - Logo"></a>
+        <a class="navbar-brand" href="${base || '/'}"><img class="icono-nav" src="${base}assets/img/favicon-nav.webp" width="60" height="60" alt="Plomeros H2O Medellín - Logo"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
