12226b617370cf48cb0e7132b0ab8af1902ec63d
mateoherreno <mateoherre@outlook.es>
2026-03-11 22:27:38 -0500

    fix: navbar links use / instead of index.html to avoid Google redirect issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,14 +84,14 @@ function renderNavbar() {
     el.innerHTML = `
     <nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
       <div class="container-fluid">
-        <a class="navbar-brand" href="${base}index.html"><img class="icono-nav" src="${base}assets/img/favicon.png" alt="Plomeros H2O Medellín - Logo"></a>
+        <a class="navbar-brand" href="${base || '/'}"><img class="icono-nav" src="${base}assets/img/favicon.png" alt="Plomeros H2O Medellín - Logo"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item"><a class="nav-link" href="${base}index.html">Inicio</a></li>
+            <li class="nav-item"><a class="nav-link" href="${base || '/'}">Inicio</a></li>
             <li class="nav-item"><a class="nav-link" href="${base}servicios.html">Servicios</a></li>
             <li class="nav-item"><a class="nav-link" href="${base}nosotros.html">Nosotros</a></li>
             <li class="nav-item"><a class="nav-link" href="${base}blog/">Blog</a></li>
